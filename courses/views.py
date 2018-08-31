b8b518ce0dbd86f6275572c56974401a7ab26227
Pavel Palancica <pavel.palancica@gmail.com>
2018-08-31 23:43:56 +0300

    Added 404 paage for Course Not Found exceptions and optimized the code

--- courses/views.py
+++ courses/views.py
@@ -1,17 +1,17 @@
-# from django.shortcuts import render
-
-from django.http import HttpResponse
-from django.template import loader
+from django.http import Http404
+# from django.http import HttpResponse
+from django.shortcuts import render
+# from django.template import loader
 from .models import Course
 
 # Create your views here.
 
 def index(request):
     all_courses = Course.objects.all()
-    template = loader.get_template('courses/index.html')
-    context = {
-        'all_courses' : all_courses
-    }
+    # template = loader.get_template('courses/index.html')
+    context = { 'all_courses' : all_courses }
+
+    return render(request, 'courses/index.html', context)
 
     # html = ''
     #
@@ -19,7 +19,14 @@ def index(request):
     #     url = '/courses/' + str(course.id) + '/'
     #     html += '<a href="' + url + '">' + course.course_title + '</a><br/>'
 
-    return HttpResponse(template.render(context, request))
+    # return HttpResponse(template.render(context, request))
 
 def detail(request, course_id):
-    return HttpResponse("<h2>Details for Course id: " + str(course_id) + "</h2>")
+    try:
+        course = Course.objects.get(pk=course_id)
+    except Course.DoesNotExist:
+        raise Http404("Album does not exist")
+
+    return render(request, 'courses/detail.html', { 'course' : course })
+
+    # return HttpResponse("<h2>Details for Course id: " + str(course_id) + "</h2>")
