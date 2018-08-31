aa6af4be42f8527311c827d36d37acf9a9115df2
Pavel Palancica <pavel.palancica@gmail.com>
2018-08-31 23:27:32 +0300

    Moved the html code inside a template for better separation of concerns

--- courses/views.py
+++ courses/views.py
@@ -1,19 +1,25 @@
 # from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.template import loader
 from .models import Course
 
 # Create your views here.
 
 def index(request):
     all_courses = Course.objects.all()
-    html = ''
+    template = loader.get_template('courses/index.html')
+    context = {
+        'all_courses' : all_courses
+    }
 
-    for course in all_courses:
-        url = '/courses/' + str(course.id) + '/'
-        html += '<a href="' + url + '">' + course.course_title + '</a><br/>'
+    # html = ''
+    #
+    # for course in all_courses:
+    #     url = '/courses/' + str(course.id) + '/'
+    #     html += '<a href="' + url + '">' + course.course_title + '</a><br/>'
 
-    return HttpResponse(html)
+    return HttpResponse(template.render(context, request))
 
 def detail(request, course_id):
     return HttpResponse("<h2>Details for Course id: " + str(course_id) + "</h2>")
