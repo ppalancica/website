5f59c5c8315850dc3b668bee5d4e237f2b437213
Pavel Palancica <pavel.palancica@gmail.com>
2018-09-01 01:03:31 +0300

    Some modifications for better maintenance in the future

--- courses/urls.py
+++ courses/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'courses'
+
 urlpatterns = [
     # /courses/
     path('', views.index, name='index'),
--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,6 @@
-from django.http import Http404
+# from django.http import Http404
 # from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 # from django.template import loader
 from .models import Course
 
@@ -22,10 +22,12 @@ def index(request):
     # return HttpResponse(template.render(context, request))
 
 def detail(request, course_id):
-    try:
-        course = Course.objects.get(pk=course_id)
-    except Course.DoesNotExist:
-        raise Http404("Album does not exist")
+    course = get_object_or_404(Course, pk=course_id)
+
+    # try:
+    #     course = Course.objects.get(pk=course_id)
+    # except Course.DoesNotExist:
+    #     raise Http404("Album does not exist")
 
     return render(request, 'courses/detail.html', { 'course' : course })
 
