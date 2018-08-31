3d447fbcefe32b2d8e95c44c910bcf0628e09a0d
Pavel Palancica <pavel.palancica@gmail.com>
2018-08-31 23:10:08 +0300

    Mixed some html in the python code, in order to display the list of courses and each individual course titlea

--- courses/urls.py
+++ courses/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # /courses/
     path('', views.index, name='index'),
+
+    # /courses/35
+    # url(r'^(?P<course_id>[0-9]+)/$')
+    path('<int:course_id>/', views.detail, name='detail')
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,8 +1,19 @@
 # from django.shortcuts import render
 
 from django.http import HttpResponse
+from .models import Course
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>This is the Courses app homepage</h1>")
+    all_courses = Course.objects.all()
+    html = ''
+
+    for course in all_courses:
+        url = '/courses/' + str(course.id) + '/'
+        html += '<a href="' + url + '">' + course.course_title + '</a><br/>'
+
+    return HttpResponse(html)
+
+def detail(request, course_id):
+    return HttpResponse("<h2>Details for Course id: " + str(course_id) + "</h2>")
