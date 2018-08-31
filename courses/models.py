b185716b3979062cc38f8c653a9a66cf0b2a7e32
Pavel Palancica <pavel.palancica@gmail.com>
2018-08-31 21:35:27 +0300

    Added Course and Lesson models

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+    author = models.CharField(max_length=255)
+    course_title = models.CharField(max_length=255)
+    technology = models.CharField(max_length=100)
+    course_logo = models.CharField(max_length=1000)
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    file_type = models.CharField(max_length=10)
+    lesson_title = models.CharField(max_length=255)
